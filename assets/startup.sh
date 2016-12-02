ad9d3d0bb70da9eec7179d3ba7997080312b55ec
qwazer <resheto@gmail.com>
2016-12-02 22:12:43 +0300

    -h myhost

--- assets/startup.sh
+++ assets/startup.sh
@@ -14,10 +14,10 @@ LISTENER_ORA=/u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
 TNSNAMES_ORA=/u01/app/oracle/product/11.2.0/xe/network/admin/tnsnames.ora
 
 cp "${LISTENER_ORA}.tmpl" "$LISTENER_ORA" &&
-sed -i "s/%hostname%/172.17.0.2/g" "${LISTENER_ORA}" &&
+sed -i "s/%hostname%/$HOSTNAME/g" "${LISTENER_ORA}" &&
 sed -i "s/%port%/1521/g" "${LISTENER_ORA}" &&
 cp "${TNSNAMES_ORA}.tmpl" "$TNSNAMES_ORA" &&
-sed -i "s/%hostname%/172.17.0.2/g" "${TNSNAMES_ORA}" &&
+sed -i "s/%hostname%/$HOSTNAME/g" "${TNSNAMES_ORA}" &&
 sed -i "s/%port%/1521/g" "${TNSNAMES_ORA}" &&
 
 service oracle-xe start
